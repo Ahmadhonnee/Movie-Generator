5b51bf77b19d2b8cf86863ff6806f9af9add5e07
shuhratovahmadhon <shuhratovahmadhon@gmail.com>
2022-09-01 16:11:05 +0500

    Render

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@ var siteContainer = document.querySelector(".container");
         <div class="card-body">
             lorem
         </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">An item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+        </ul>
     </div>
 </div> */}
 
@@ -23,6 +28,10 @@ function createListOfMovies(movie){
     let cardEl = document.createElement('div');
     cardEl.className = "card myCard";
 
+    let cardImgEl = document.createElement('img');
+    cardImgEl.className = "card-img-top"
+    cardImgEl.setAttribute("src", "https://picsum.photos/500/500")
+
     // card element's header
     let  cardHeaderEl = document.createElement('div');
     cardHeaderEl.className = "card-header";
@@ -36,18 +45,66 @@ function createListOfMovies(movie){
     cardBodyEl.className = "card-body" ;
     cardBodyEl.textContent = movie.description;
 
+    // card elements' list
+    let cardListEl = document.createElement('ul');
+    cardListEl.className = "list-group list-group-flush";
+
+    // cardListEl ' s li 1
+    let cardListItemEl1 = document.createElement('li');
+    cardListItemEl1.className = "list-group-item";
+
+    // cardListEl ' s li 2
+    let cardListItemEl2 = document.createElement('li');
+    cardListItemEl2.className = "list-group-item";
+
+    // cardListEl ' s li 3
+    let cardListItemEl3 = document.createElement('li');
+    cardListItemEl3.className = "list-group-item";
+
+    // cardListEl ' s li 4
+    let cardListItemEl4 = document.createElement('li');
+    cardListItemEl4.className = "list-group-item";
+
+    // cardListEl ' s li 5
+    let cardListItemEl5 = document.createElement('li');
+    cardListItemEl5.className = "list-group-item";
+
+
+    for(let i = 0; i < movies.length; i++){
+        // let cardListItemElCUSTOM = `cardListItemEl${i}`;
+        cardListItemEl1.textContent = movies[i].id;
+        cardListItemEl2.textContent = movies[i].year;
+        cardListItemEl3.textContent = movies[i].director;
+        cardListItemEl3.textContent = movies[i].description;
+    }
 
     // SO now we are creating right DOM elements
 
     // <col> parent
     // <div class="card">
     colEL.appendChild(cardEl);
+    // <img class="card-img-top"></img>
+    cardEl.appendChild(cardImgEl);
     // <div class="card-header">
     cardEl.appendChild(cardHeaderEl);
     // <h4>Title</h4>
     cardHeaderEl.appendChild(cardTitleEl);
     // <div class="card-body">lorem</div>
     cardEl.appendChild(cardBodyEl);
+    // <ul class="list-group list-group-flush">
+    cardEl.appendChild(cardListEl);
+    // <li class="list-group-item">An item</li>
+    cardListEl.appendChild(cardListItemEl1);
+    // <li class="list-group-item">An item</li>
+    cardListEl.appendChild(cardListItemEl2);
+    // <li class="list-group-item">An item</li>
+    cardListEl.appendChild(cardListItemEl3);
+    // <li class="list-group-item">An item</li>
+    cardListEl.appendChild(cardListItemEl4);
+    // <li class="list-group-item">An item</li>
+    cardListEl.appendChild(cardListItemEl5);
+
+
     
     
     // Here it returns 
